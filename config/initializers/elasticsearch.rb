56cf8181f068a1f15c1e4d3c02c92427acc34445
shroomcrumpet <kencheng3@gmail.com>
2018-11-07 06:20:50 +0800

    Re-enabled elasticsearch.rb config; added heroku config for searchkick (heroku config:set ELASTICSEARCH_URL=https://j3b9b8yy4i:tb40jhzmck@maple-2245918.us-east-1.bonsaisearch.net)

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,7 +1,7 @@
-# BONSAI_URL = ENV['BONSAI_URL']
-#  if BONSAI_URL
-#   Elasticsearch::Model.client = Elasticsearch::Client.new(
-#     url: BONSAI_URL,
-#     log: true
-#   )
-# end
+BONSAI_URL = ENV['BONSAI_URL']
+ if BONSAI_URL
+  Elasticsearch::Model.client = Elasticsearch::Client.new(
+    url: BONSAI_URL,
+    log: true
+  )
+end
